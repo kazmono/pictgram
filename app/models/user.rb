9b5c258f60a01fd7bf12834de7dc29eb9c6530b9
名前 <>
2021-07-09 05:25:26 +0000

    20210709rails05課題

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
+  validates :name, presence: true, length: {minimum: 1, maximum: 15}
+  validates :email, presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+  validates :password, presence: true, length: {in: 8..32}, format: {with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,12}\z/}
   
   has_secure_password
   
